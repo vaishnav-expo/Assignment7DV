b9c4da6aa4e69c12ff238ea1a835bd64a1950b65
Vaishnav <vpkshirsagar@dons.usfca.edu>
2020-10-04 16:44:38 -0700

    Done with all three charts modified.

--- AreaChart.js
+++ AreaChart.js
@@ -1,23 +1,16 @@
-// set the dimensions and margins of the graph
 var margin = {top: 40, right: 20, bottom: 30, left: 80},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-// parse the date / time
 var parseTime = d3.timeParse("%Y");
 
-// set the ranges
 var x = d3.scaleTime().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
 
-// define the line
 var valueline = d3.line()
     .x(function(d) { return x(d["Year"]); })
     .y(function(d) { return y(d["Emissions.Type.CO2"]); });
 
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom + 100)
@@ -28,14 +21,12 @@ var svg = d3.select("body").append("svg")
 d3.csv("emissions.csv").then((data) => {
     data = data.filter(function(d, i){ return d["Country"] === "Afghanistan" })
 
-    // format the data
     data.forEach(function(d) {
         d["Year"] = parseTime(d["Year"]);
         d["Emissions.Type.CO2"] = +d["Emissions.Type.CO2"];
     });
     console.log(data);
 
-    // Scale the range of the data
     x.domain(d3.extent(data, function(d) { return d["Year"]; }));
     y.domain([0, d3.max(data, function(d) { return d["Emissions.Type.CO2"] })]);
 
